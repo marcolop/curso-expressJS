64c5a6348c988a5cbc04ef4f09d1499e5f92e50c
marcolop <jacobo__1@hotmail.com>
2025-05-14 19:21:20 -0600

    guardando cambios de post y get en usuarios y validacion de email y name

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
+const fs = require('fs');
+const path = require('path');
+const usersFilePath = path.join(__dirname, 'users.json');
 
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:  true}));
 
@@ -44,6 +47,54 @@ app.post('/form', (req, res ) => {
    })
 })
 
+app.post('/api/data', (req, res) => {
+  const data = req.body;
+
+  if (!data || Object.keys(data).length === 0) {
+     return res.status(400).json({eror: 'no se recibieron datos'});
+  }
+
+  res.status(201).json({
+   message: 'Datos JSON recibidos',
+   data
+  })
+})
+
+app.get('/users', (req, res) => {
+   fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+      if (err) {
+         return res.status(500).json({ error: 'Error con la conexion de datos.' })
+      }
+      const users = JSON.parse(data);
+      res.json(users);
+   })
+});
+
+app.post('/users', (req, res) => {
+   const newUser = req.body;
+   // validacion de nombre
+   if(!newUser.name || newUser.name.length < 3) {
+      return res.status(400).json({error: 'El nombre debe tener al menos 3 caracteres'});
+   }
+   //validacion de email con expresion regular
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if (!newUser.email || !emailRegex.test(newUser.email)) {
+      return res.status(400).json({error: 'El email no es valido'})
+   }
+   fs.readFile(usersFilePath, 'utf-8', (err, data) => {
+      if (err) {
+         return res.status(500).json({ error: 'Error con conexion de datos.'})
+      }
+      const users = JSON.parse(data);
+      users.push(newUser);
+      fs.writeFile(usersFilePath, JSON.stringify(users, null, 2), (err) => {
+        if (err) {
+          return res.status(500).json({error: 'Error al guardar el usuario.'})
+        }
+        res.status(201).json(newUser);
+      })
+   });
+})
 app.listen(PORT, () => {
     console.log(`Servidor: http://localhost:${PORT}`);
 })
